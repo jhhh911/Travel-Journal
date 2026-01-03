0ebd7d1bd0f6c108fcdc4ebcdb535374cac0e417
James Heiniger <james.heiniger911@gmail.com>
2026-01-03 20:58:20 +1030

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import Entry from './components/Entry'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <Entry />
+      </main>
+    </>
+  )
+}
+
