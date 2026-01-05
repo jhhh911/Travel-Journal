ce2bcd1b2f85b7e97ad518f54f23a72f752748d9
James Heiniger <james.heiniger911@gmail.com>
2026-01-05 14:33:24 +1030

    Now importing data and generating content.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import Header from './components/Header'
-import Entry from './components/Entry'
+import Header from "./components/Header";
+import Entry from "./components/Entry";
+import data from './data.js'
 
 export default function App() {
+  const entryElements = data.map(entry => {
+    return <Entry 
+          img={entry.img}
+          country={entry.country}
+          googleMapsLink={entry.googleMapsLink}
+          title={entry.title}
+          dates={entry.dates}
+          text={entry.text}
+    />
+  })
   return (
     <>
       <Header />
       <main className="container">
-        <Entry />
+        {entryElements}
       </main>
     </>
-  )
+  );
 }
-
